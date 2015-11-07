ca35319434525003899323cc2a1c1fac29292c27
Jhinel Arcaya <proyectoamicoro@gmail.com>
2015-11-07 13:29:38 -0430

    Nothing relevant.

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ var express = require('express'),
 app = express(),
 twitter = require('./src/server-module.js'),
 url = 'https://api.twitter.com/1.1/statuses/mentions_timeline.json',
-counter = 6,
+counter = 6, port = 8009
 getUrl = url + '?' + 'count=' + counter
 
 app.post('/', function (req, res){
 	res.send(twitter(getUrl))
 })
 
-app.listen('8009', function(){
-	console.log('server successfully on localhost:8009')
+app.listen(port, function(){
+	console.log('server successfully on localhost:' + port)
 })
