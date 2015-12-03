988d3bebdabad2467b5cd12e813545b7c509c8a6
Jhinel Arcaya <proyectoamicoro@gmail.com>
2015-12-03 11:19:08 -0430

    Request header done.

--- src/lib/local-server.js
+++ src/lib/local-server.js
@@ -1,36 +1,24 @@
 var Oauth = require('oauth-1.0a'),
-	http = require('http');
-var oauth = Oauth({
+	http = require('http'),
+	fs = require('fs')
+	oauth = Oauth(options);
+var options = {
 	consumer: {
-		public: 'FjNnsIOSMjYd03iEZEVJaaWPZ',
-		secret: 'PPIZ72Y0HIQkzq9GoTRVlQV3LzW5GC3ywSGSB7curYYBCLlHzM'
+		public: "FjNnsIOSMjYd03iEZEVJaaWPZ",
+		secret: "PPIZ72Y0HIQkzq9GoTRVlQV3LzW5GC3ywSGSB7curYYBCLlHzM"
 	},
-	signature_method: 'HMAC-SHA1'
-}),
-
+	signature-method: "HMCA-SHA1",
+	version: "1.0"
+},
 token = {
-	public: '102399145-RfsdvLWRQFwLbrqebv5KnWccJNwnw1HKK8X5zTcj',
-	secret: 'EKYDXf481H1mps6Si9g49aTUe2eviVELrpGQwR705cIP1'
+		puclic: "102399145-RfsdvLWRQFwLbrqebv5KnWccJNwnw1HKK8X5zTcj",
+		secret: "EKYDXf481H1mps6Si9g49aTUe2eviVELrpGQwR705cIP1"
+	},
+tweet_req = {
+	method: 'GET',
+	url: '',
+	data: ''
 };
 
-var request_data = {
-	url: 'https://api.twitter.com/statuses/mentions_timeline.json',
-	method: 'GET',
-	data: {
-		oauth_version: '1.0'
-	}
-},
-
-req = http.request({url: request_data.url,
-	method: request_data.method,
-	form: request_data.data,
-	header: oauth.toHeader(oauth.authorize(request_data, token))
-	}, function(res){
-		console.log(res.statusCode);
-		console.log(JSON.stringify(res.headers));
-		res.on('data', function(chunk){
-			console.log('BODY: ' + chunk);
-		});
-	}).on('error', function(err){console.error(err.message);});
-req.write('data\n');
-req.end();
+//The header
+oauth.toHeader(oauth.authorize(tweet_req, token));
