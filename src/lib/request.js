6b67ad5c155988a52e181716793c93bfc147427c
Jhinel Arcaya <proyectoamicoro@gmail.com>
2015-12-05 14:06:01 -0430

    Fixed errors.

--- src/lib/request.js
+++ src/lib/request.js
@@ -1,18 +1,19 @@
 var Oauth = require('oauth-1.0a'),
-	oauth = Oauth(options);
-var options = {
+	options = {
 	consumer: {
 		public: "FjNnsIOSMjYd03iEZEVJaaWPZ",
 		secret: "PPIZ72Y0HIQkzq9GoTRVlQV3LzW5GC3ywSGSB7curYYBCLlHzM"
 	},
-	signature-method: "HMCA-SHA1",
+	signature_method: "HMAC-SHA1",
 	version: "1.0"
 },
-
-token = {
+	
+	token = {
 	puclic: "102399145-RfsdvLWRQFwLbrqebv5KnWccJNwnw1HKK8X5zTcj",
 	secret: "EKYDXf481H1mps6Si9g49aTUe2eviVELrpGQwR705cIP1"
-};
+},
+
+	oauth = Oauth(options);
 
 //The header
 module.exports = function(req_data) {
